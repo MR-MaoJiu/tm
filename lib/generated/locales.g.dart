6f0195b26064fed6fde913c193c85030c59ec6bc
猫九先森 <481869314@qq.com>
2022-10-05 19:44:29 +0800

    国际化tm

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,45 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'en_US': Locales.en_US,
+    'zh_CN': Locales.zh_CN,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const buttons_register = 'buttons_register';
+  static const buttons_login = 'buttons_login';
+  static const buttons_logout = 'buttons_logout';
+  static const buttons_sign_in_wechat = 'buttons_sign_in_wechat';
+  static const buttons_sign_in_apple = 'buttons_sign_in_apple';
+  static const text_tm = 'text_tm';
+  static const text_mine = 'text_mine';
+  static const text_home = 'text_home';
+}
+
+class Locales {
+  static const en_US = {
+    'buttons_register': 'Register',
+    'buttons_login': 'Login',
+    'buttons_logout': 'Logout',
+    'buttons_sign_in_wechat': 'Sign-in with WeChat',
+    'buttons_sign_in_apple': 'Sign-in with Apple',
+    'text_tm': 'ten metres',
+    'text_mine': 'mine',
+    'text_home': 'home',
+  };
+  static const zh_CN = {
+    'buttons_register': '注册',
+    'buttons_login': '登录',
+    'buttons_logout': '登出',
+    'buttons_sign_in_wechat': '微信登录',
+    'buttons_sign_in_apple': '苹果登录',
+    'text_tm': '十米',
+    'text_mine': '我的',
+    'text_home': '首页',
+  };
+}
